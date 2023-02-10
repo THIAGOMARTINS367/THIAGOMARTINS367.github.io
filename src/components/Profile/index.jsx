d233158bc7e03a095600730f35bb28530673d3d8
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-02-10 14:04:48 -0300

    feat(src/components/Profile/index.jsx): Componente "Profile", para exibir informações de "perfil", criado e implementado

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,36 @@
+import './index.css';
+import data from '../../database/data.json';
+
+function Profile() {
+  const { profile, profile: { description } } = data;
+  return (
+    <section className="section-profile">
+      <div className="div-description">
+        { 
+          description.paragraphs.map((paragraph) => (
+            <p className="p-description" dangerouslySetInnerHTML={{__html: paragraph}}></p>
+          ))
+        }
+      </div>
+
+      <section className="section-informations">
+        <div>
+          <img src={ profile.picture } alt="imagem de perfil" />
+        </div>
+        <div>
+          {
+            profile.contacts.map((contactObj) => (
+              <div key={ contactObj.name } className="div-profile-icons">
+                <a href={ contactObj.url } target="_blank">
+                  <img className="profile-icons" src={ contactObj.image } alt={ contactObj.name } />
+                </a>
+              </div>
+            ))
+          }
+        </div>
+      </section>
+    </section>
+  );
+}
+
+export default Profile;
