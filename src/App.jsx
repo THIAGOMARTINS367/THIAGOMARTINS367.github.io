99fb8168dc3d8fa0ca8ceef55646a74f10ac583c
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-02-10 14:11:17 -0300

    feat(src/App.jsx): Rota "/" que redireciona para a página "Home", criada e implementada

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <section className="App">
+      <Routes>
+        <Route path="/"  element={<Home />} />
+      </Routes>
+    </section>
+  );
+}
+
+export default App;
