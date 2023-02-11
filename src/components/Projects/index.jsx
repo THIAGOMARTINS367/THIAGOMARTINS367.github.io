64d110736d1fda48ec721b4d538b56a7dd969683
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-02-11 13:08:42 -0300

    feat(src/components/Projects/index.jsx): Atribuição de classes às tag's para implementação do CSS da página

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,43 +1,55 @@
 import data from '../../database/data.json';
+import './index.css';
 
 function Projects() {
   const { projectsSection } = data;
   return (
-    <section>
-      <section>
+    <section className="section-projects">
+      <h2 className="h2-section-projects">
+        { projectsSection.title.toUpperCase() }
+      </h2>
+      <section className="section-project-cards">
         {
           projectsSection.projects.map((projectObj) => (
-            <div>
-              <div>
-                <img src={ projectObj.image } alt={ projectObj.title } />
+            <div key={ projectObj.title } className="div-project-card">
+              <div className="div-img-project-card">
+                <img className="img-project-card" src={ projectObj.image } alt={ projectObj.title } />
               </div>
-              <div>
-                <h3>{ projectObj.title }</h3>
+              <section className="section-project-card-information">
                 <div>
-                  <p>{ projectObj.technologies }</p>
+                  <h3 className="h3-project-card-title">{ projectObj.title }</h3>
+                  <div className="div-project-card-technologies">
+                    {
+                      projectObj.technologies.map((technology) => (
+                        <div key={ technology } className="div-project-card-technology">{ technology }</div>
+                      ))
+                    }
+                  </div>
                 </div>
-              </div>
-              <div>
-                <p>{ projectObj.description }</p>
-              </div>
-              <div>
                 <div>
-                  {
-                    projectObj.repository && (
-                      <a href="#" target="_blank">
-                        <button type="button">Github</button>
-                      </a>
-                    )
-                  }
-                  {
-                    projectObj.url && (
-                      <a href="#" target="_blank">
-                        <button type="button">Ver</button>
-                      </a>
-                    )
-                  }
+                  <p className="p-project-card-description">{ projectObj.description }</p>
                 </div>
-              </div>
+                <div className="div-card-buttons">
+                  <div className="div-button-repository">
+                    {
+                      projectObj.repository && (
+                        <a href="#" target="_blank">
+                          <button type="button">Github</button>
+                        </a>
+                      )
+                    }
+                  </div>
+                  <div className="div-button-webpage">
+                    {
+                      projectObj.url && (
+                        <a href="#" target="_blank">
+                          <button type="button">Ver</button>
+                        </a>
+                      )
+                    }
+                  </div>
+                </div>
+              </section>
             </div>
           ))
         }
