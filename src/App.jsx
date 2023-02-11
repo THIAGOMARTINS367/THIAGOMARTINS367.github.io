4622cc5b8624369b3cc8f5a73fb667b20022128e
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-02-11 14:13:30 -0300

    style(src/App.jsx): :art: Alterações feitas para corrigir formatação do código

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <section className="App">
       <Routes>
-        <Route path="/"  element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </section>
   );
