9c6a1f30c475190ce0cecf106692341af7066aac
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-02-12 16:26:49 -0300

    feat(src/components/About/index.jsx): Componente "About", para exibir informações pessoais de perfil, criado e implementado

---
+++ src/components/About/index.jsx
@@ -0,0 +1,20 @@
+import data from '../../database/data.json';
+import './index.css';
+
+function About() {
+  const { profile: { description } } = data;
+  return (
+    <section className="section-about">
+      <div className="div-about-description">
+        {
+          description.paragraphs.map((paragraph) => (
+            <p key={ paragraph } dangerouslySetInnerHTML={{ __html: paragraph }}></p>
+          ))
+        }
+      </div>
+      
+    </section>
+  );
+}
+
+export default About;
