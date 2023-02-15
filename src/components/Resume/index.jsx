b1832caab163ed2d4c66242e3ea1a33f5adc84dc
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-02-14 23:44:59 -0300

    feat(src/components/Resume/index.jsx): Componente "Resume", para exibir resumo das principais informações de conhecimento e experiência, criado e implementado

---
+++ src/components/Resume/index.jsx
@@ -0,0 +1,71 @@
+import data from '../../database/data.json';
+import './index.css';
+
+function Resume () {
+  const { professional } = data;
+  return (
+    <section className="section-resume">
+      <section className="timeline">
+        <div className="div-title-img-timeline">
+          <div className="div-img-icon-education">
+            <img src="/assets/stacked-book.svg" />
+          </div>
+          <h3 className="h3-title-timeline">
+            Education
+          </h3>
+        </div>
+        <ul className="ul-list-timeline">
+          <li className="li-timeline-bottom-line-icon"><div></div></li>
+          {
+            professional.education.map((itemObj) => (
+              <li className="li-timeline-item">
+                <div className="div-marker-separator">
+                  <div className="div-timeline-item-marker">
+                    <div></div>
+                  </div>
+                </div>
+                <div className="div-timeline-item-content">
+                  <h4>{ itemObj.title }</h4>
+                  <span className="item-timeline-period">{ itemObj.start } — { itemObj.end }</span>
+                  <p dangerouslySetInnerHTML={{ __html: itemObj.description }}></p>
+                </div>
+              </li>
+            ))
+          }
+        </ul>
+      </section>
+
+      <section className="timeline">
+        <div className="div-title-img-timeline">
+          <div className="div-img-icon-experience">
+            <img src="/assets/user-settings.svg" />
+          </div>
+          <h3 className="h3-title-timeline">
+            Experience
+          </h3>
+        </div>
+        <ul className="ul-list-timeline">
+          <li className="li-timeline-bottom-line-icon"><div></div></li>
+          {
+            professional.experience.map((itemObj) => (
+              <li className="li-timeline-item">
+                <div className="div-marker-separator">
+                  <div className="div-timeline-item-marker">
+                    <div></div>
+                  </div>
+                </div>
+                <div className="div-timeline-item-content">
+                  <h4>{ itemObj.title }</h4>
+                  <span className="item-timeline-period">{ itemObj.start } — { itemObj.end }</span>
+                  <p dangerouslySetInnerHTML={{ __html: itemObj.description }}></p>
+                </div>
+              </li>
+            ))
+          }
+        </ul>
+      </section>
+    </section>
+  );
+}
+
+export default Resume;
